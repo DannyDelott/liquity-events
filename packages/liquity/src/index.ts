c74556a91572f56182198339b6e084e04e6a36c6
Danny Delott <dannydelott@gmail.com>
2023-01-28 18:24:08 -0800

    Fix build

--- packages/liquity/src/index.ts
+++ packages/liquity/src/index.ts
@@ -3,12 +3,15 @@ export * from "src/contracts/lqtyStaking";
 
 export * from "src/borrow/borrowInfos";
 export * from "src/borrow/makeBorrowInfosFile";
+export * from "src/borrow/s3";
 
 export * from "src/stake/stakeChangedInfos";
 export * from "src/stake/makeStakeChangedInfosFile";
+export * from "src/stake/s3";
 
 export * from "src/redeem/redemptionInfos";
 export * from "src/redeem/makeRedemptionInfosFile";
+export * from "src/redeem/s3";
 
 export * from "src/lqtyToken";
 export * from "src/lusdToken";
