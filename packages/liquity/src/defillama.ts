7d4200162f91959203e9961fc76774ebd723801c
Danny Delott <dannydelott@gmail.com>
2023-01-31 09:38:36 -0800

    Add batchFetchPrice method

--- packages/liquity/src/defillama.ts
+++ packages/liquity/src/defillama.ts
@@ -1,9 +1,6 @@
 import fetch from "node-fetch";
 export interface PriceInfo {
-  decimals: number;
-  symbol: string;
   price: number; // in USD
-  timestamp: number; // in milliseconds;
 }
 
 interface FetchPriceResponse {
@@ -12,6 +9,11 @@ interface FetchPriceResponse {
     [id: string]: PriceInfo;
   };
 }
+
+export function makeTokenId(tokenAddress: string) {
+  return `ethereum:${tokenAddress}`;
+}
+
 export async function fetchEthPrice(timestampMS?: number): Promise<PriceInfo> {
   const tokenId = `coingecko:ethereum`;
   const priceUrl = timestampMS
@@ -27,12 +29,42 @@ export async function fetchPrice(
   tokenAddress: string,
   timestampMS?: number
 ): Promise<PriceInfo | undefined> {
-  const tokenId = `ethereum:${tokenAddress}`;
+  const tokenId = makeTokenId(tokenAddress);
   const priceUrl = timestampMS
     ? `https://coins.llama.fi/prices/historical/${timestampMS}/${tokenId}`
     : `https://coins.llama.fi/prices/current/${tokenId}`;
 
+  console.log("priceUrl", priceUrl);
   const response = await fetch(priceUrl);
+  console.log("response", response.status, response.statusText);
   const json = (await response.json()) as FetchPriceResponse;
   return json.coins[tokenId];
+}
+
+type BatchPriceInfo = Record<
+  // id defined as {chain}:{address}
+  string,
+  {
+    prices: PriceInfo[];
+  }
+>;
+
+interface FetchBatchPriceResponse {
+  coins: BatchPriceInfo;
+}
+export async function fetchBatchHistoricalPrice(
+  tokenAddresses: string[],
+  timestamps: number[]
+): Promise<BatchPriceInfo> {
+  const batchPriceRequestObject: Record<string, number[]> = {};
+  tokenAddresses.forEach((tokenAddress) => {
+    batchPriceRequestObject[makeTokenId(tokenAddress)] = timestamps;
+  });
+
+  const encoded = encodeURI(JSON.stringify(batchPriceRequestObject));
+  const priceUrl = `https://coins.llama.fi/batchHistorical?coins=${encoded}`;
+  const response = await fetch(priceUrl);
+  console.log("response", response.status, response.statusText);
+  const json = (await response.json()) as FetchBatchPriceResponse;
+  return json.coins;
 }
