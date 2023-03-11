25f925c41b28908a6c26176266f35b04c3066547
Danny Delott <dannydelott@gmail.com>
2023-03-11 10:19:01 -0800

    Add price to title tag

--- apps/liquity-ui/pages/index.tsx
+++ apps/liquity-ui/pages/index.tsx
@@ -1,6 +1,18 @@
+import { LQTY_TOKEN_ADDRESS } from "liquity";
+import Head from "next/head";
 import { ReactElement } from "react";
 import { HomeView } from "src/ui/home/HomeView/HomeView";
+import { useTokenPrice } from "src/ui/price/useTokenPrice";
 
 export default function IndexPage(): ReactElement {
-  return <HomeView />;
+  const { data: price } = useTokenPrice(LQTY_TOKEN_ADDRESS);
+
+  return (
+    <>
+      <Head>
+        <title>${price?.price.toFixed(2)} | Liquity.events</title>
+      </Head>
+      <HomeView />
+    </>
+  );
 }
